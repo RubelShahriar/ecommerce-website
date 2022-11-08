b641e63dd7ff1d77264c97978f9cc7e2272d03b2
RubelShahriar <mr.rubelshahriar@gmail.com>
2022-11-08 20:46:52 +0600

    payment gateway

---
+++ components/HeroBanner.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import Link from "next/link";
+// local
+import { urlFor } from "../lib/client";
+
+const HeroBanner = ({ heroBanner }) => {
+  const {
+    smallText,
+    midText,
+    largeText1,
+    largeText2,
+    image,
+    product,
+    buttonText,
+    desc,
+  } = heroBanner;
+  return (
+    <div className="hero-banner-container">
+      <div>
+        <p>{smallText}</p>
+        <h3>{midText}</h3>
+        <h1>{largeText1}</h1>
+        {largeText2}
+        <img
+          src={urlFor(image)}
+          alt="headphones"
+          className="hero-banner-image"
+        />
+        <div>
+          <Link href={`/product/${product}`}>
+            <button type="button">{buttonText}</button>
+          </Link>
+          <div className="desc">
+            <h5>Description</h5>
+            <p>{desc}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HeroBanner;
